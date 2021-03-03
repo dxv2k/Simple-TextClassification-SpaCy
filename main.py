d93c35531f577c8825093596c14daa06640adabb
dxv2k <9569076+dxv2k@users.noreply.github.com>
2021-03-03 18:33:48 +0700

    add requirements, init main, work in progress

---
+++ main.py
@@ -0,0 +1,14 @@
+import pandas 
+import spacy 
+
+data_path = './data/yelp_ratings.csv'
+
+
+def load_data(data_path, split = 0.85): 
+    data = pd.read_csv(data_path)
+    # shuffle data 
+    return 
+
+
+# def main(): 
+
